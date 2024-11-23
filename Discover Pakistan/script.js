f318eb2fc1fbdc95f144e1abf4a4c1a223efb397
M-Wasil <wasilpriv.24@gmail.com>
2024-11-23 20:15:29 +0500

    Update script.js

--- Discover Pakistan/script.js
+++ Discover Pakistan/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const emailLinks = document.querySelectorAll('.footer-contact a');
-    
     emailLinks.forEach(link => {
       link.addEventListener('click', (event) => {
         event.preventDefault(); // Prevent default mailto behavior
@@ -8,5 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
         window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`, '_blank');
       });
     });
+    const bookNowButton = document.querySelector('.book-now');
+    const learnMoreButton = document.querySelector('.learn-more');
+
+  // Add click event listeners
+  bookNowButton.addEventListener('click', () => {
+    window.location.href = 'Booking.html'; // Replace with your Book Now page
+  });
+
+  learnMoreButton.addEventListener('click', () => {
+    window.location.href = 'learnMore.html'; // Replace with your Learn More page
+  });
   });
 
